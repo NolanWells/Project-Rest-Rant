9c7ea601a0a96090c48b54d22eac4e024df564d1
unknown <nolan@thewellsfamily.us>
2023-03-23 16:43:32 -0700

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,9 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
+
+router.get('/', (req, res) => {
+    res.render('places/index', { places })
+})
 
 router.get('/', (req, res) => {
     let places = [{
@@ -20,5 +25,10 @@ router.get('/', (req, res) => {
     })
     //res.send('GET /places')
 })
+router.post('/', (req, res) => {
+  console.log(req.body)
+  res.send('POST /places')
+})
+
 
 module.exports = router
