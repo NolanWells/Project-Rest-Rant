5d866583eafd8bfbc97de70cc413062c10518c0c
unknown <nolan@thewellsfamily.us>
2023-03-29 22:12:50 -0700

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -15,6 +15,20 @@ router.post('/', (req, res) => {
   res.redirect('/places')
 })
 
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id] })
+  }
+})
+
+
 
 router.get('/:id', (req, res) => {
   let id = Number(req.params.id)
@@ -63,7 +77,7 @@ router.delete('/:id', (req, res) => {
     res.render('error404')
   }
   else {
-    places.splice(id,1)
+    places.splice(id, 1)
     res.render('/places')
   }
 })
