301f3d0e81f7467fd92be85dd22e23de7a1a1c63
unknown <nolan@thewellsfamily.us>
2023-03-29 21:41:07 -0700

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -16,9 +16,22 @@ router.post('/', (req, res) => {
 })
 
 
-router.get('/', (req, res) => {
-    res.render('places/index', { places })
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if(isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]){
+    res.render('error404')
+  }
+  else{
+    res.render('places/show', { places: places[id] })
+  }
 })
+
+// router.get('/', (req, res) => {
+//     res.render('places/index', { places })
+// })
 
 router.get('/', (req, res) => {
     let places = [{
